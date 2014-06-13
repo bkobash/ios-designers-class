15bfd5d746508b7709dcd2278f486ac638630bec
Brian Kobashikawa <bkobash@yahoo-inc.com>
2014-06-12 22:51:16 -0700

    Added drill files for 6/12

---
+++ wk2-drill/warmup-week2/LoadingViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoadingViewController.h
+//  warmup-week2
+//
+//  Created by Brian Kobashikawa on 6/12/14.
+//  Copyright (c) 2014 Brian Kobashikawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//@interface LoadingViewController : UIViewController
+
+// when you want to enable delegates for the alert popup
+@interface LoadingViewController : UIViewController <UIAlertViewDelegate, UIActionSheetDelegate>
+
+@end
